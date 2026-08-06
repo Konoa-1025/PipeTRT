7ae6ec1fe6d6941685f223ff8b590e3c6e5c4577
Norifumi Kondo <norifumi.kondo.it@gmail.com>
2026-08-06 21:29:04 +0900

    update: test_onnxのランドマーク表示の追加

--- scripts/test_onnx.py
+++ scripts/test_onnx.py
@@ -6,7 +6,6 @@ sys.path.insert(0, str(PROJECT_ROOT))
 
 from pipetrt.onnx_inference import ONNXInference
 
-
 IMAGE_PATH = PROJECT_ROOT / "samples" / "hand.jpg"
 
 
@@ -16,7 +15,23 @@ def main():
 
     outputs = model.infer(IMAGE_PATH)
 
+    # ===== 追加ここから =====
+    landmarks = outputs[0].reshape(21, 3)
+
+    print("=== LANDMARKS ===")
+
+    for index, landmark in enumerate(landmarks):
+        x, y, z = landmark
+
+        print(
+            f"{index:02d}: "
+            f"x={x:8.3f}, "
+            f"y={y:8.3f}, "
+            f"z={z:8.3f}"
+        )
+
     print()
+    # ===== 追加ここまで =====
 
     for index, output in enumerate(outputs):
 
