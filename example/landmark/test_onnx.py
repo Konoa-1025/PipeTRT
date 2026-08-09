54c52fa566e9ca2ea4025af7b3340a76987cbf54
Norifumi Kondo <norifumi.kondo.it@gmail.com>
2026-08-09 14:22:41 +0900

    add: onnxの保護

--- example/landmark/test_onnx.py
+++ example/landmark/test_onnx.py
@@ -6,7 +6,7 @@ import cv2
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(PROJECT_ROOT))
 
-from pipetrt.landmark.onnx_inference import ONNXInference
+from example.landmark.onnx_cpu.onnx_inference import ONNXInference
 
 
 IMAGE_PATH = PROJECT_ROOT / "samples" / "hand.jpg"
