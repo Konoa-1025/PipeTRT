4bc7bb11d30118e04901ea25882300a8da4faf2c
Norifumi Kondo <norifumi.kondo.it@gmail.com>
2026-08-09 13:10:25 +0900

    update: hand_landmarker.py

--- pipetrt/api/hand_landmarker.py
+++ pipetrt/api/hand_landmarker.py
@@ -6,6 +6,8 @@ from pipetrt.palm.anchors import generate_anchors
 from pipetrt.roi.roi import create_roi
 from pipetrt.roi.transform import extract_roi
 
+from pipetrt.onnx_inference import ONNXInference
+
 from .hand_landmarker_result import HandLandmarkerResult
 
 
@@ -13,7 +15,11 @@ class HandLandmarker:
     def __init__(self):
         self.anchors = generate_anchors()
 
+        # Landmark ONNX Runtime
+        self.landmark_model = ONNXInference()
+
     def detect(self, frame):
+        # Palm
         palm_input = preprocess(frame)
 
         boxes, scores = detect(palm_input)
@@ -28,9 +34,11 @@ class HandLandmarker:
             return HandLandmarkerResult(
                 palm_result=[],
                 roi=None,
-                roi_image=None
+                roi_image=None,
+                hand_landmarks=[]
             )
 
+        # ROI
         image_height, image_width = frame.shape[:2]
 
         roi = create_roi(
@@ -45,10 +53,24 @@ class HandLandmarker:
             output_size=224
         )
 
+        # Landmark
+        landmark_outputs = self.landmark_model.infer_frame(
+            roi_image
+        )
+
+        print("Landmark Output:")
+        for index, output in enumerate(landmark_outputs):
+            print(
+                index,
+                output.shape,
+                output
+            )
+
         return HandLandmarkerResult(
             palm_result=palm_results,
             roi=roi,
-            roi_image=roi_image
+            roi_image=roi_image,
+            hand_landmarks=[]
         )
 
     def close(self):
